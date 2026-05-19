265c8ed9ec4e516d076af762adcef4a3e01519f5
BNFLYD <moralesflaviog@gmail.com>
2026-05-19 10:39:29 -0300

    quick fix

--- frontend/src/lib/components/ui/footer.svelte
+++ frontend/src/lib/components/ui/footer.svelte
@@ -20,7 +20,7 @@
   <div class="flex w-full justify-between px-4 py-2">
     {#each sections as section}
       <button
-        class="isolate bg-accent-detail relative w-9 h-9 flex items-center justify-center rounded-md transition-all shadow-[inset_0_1px_0_rgba(255,255,255,0.10),0_2px_6px_rgba(0,0,0,0.25)] hover:shadow-[inset_0_1px_0_rgba(255,255,255,0.20),0_3px_8px_rgba(0,0,0,0.35)] active:shadow-[inset_0_4px_12px_rgba(0,0,0,0.55),inset_0_-1px_0_rgba(255,255,255,0.12)] hover:translate-y-[1px] active:translate-y-[3px] hover:bg-accent-detail/75 {active_section ===
+        class="isolate bg-accent-detail bg-inner-glow relative w-9 h-9 flex items-center justify-center rounded-md transition-all shadow-[inset_0_1px_0_rgba(255,255,255,0.10),0_2px_6px_rgba(0,0,0,0.25)] hover:shadow-[inset_0_1px_0_rgba(255,255,255,0.20),0_3px_8px_rgba(0,0,0,0.35)] active:shadow-[inset_0_4px_12px_rgba(0,0,0,0.55),inset_0_-1px_0_rgba(255,255,255,0.12)] hover:translate-y-[1px] active:translate-y-[3px] hover:bg-accent-detail/75 {active_section ===
           section.id
             ? 'btn-glow text-back'
             : 'text-back-deep'}"
