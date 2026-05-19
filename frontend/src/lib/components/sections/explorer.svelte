d5d1e66bdc6a2219939a0e73b40156ccf4de0756
BNFLYD <moralesflaviog@gmail.com>
2026-05-18 22:25:11 -0300

    message format 9

--- frontend/src/lib/components/sections/explorer.svelte
+++ frontend/src/lib/components/sections/explorer.svelte
@@ -279,7 +279,7 @@
     {/if}
   {#if deleting && delete_target}
     <div class="">
-      <div class="bg-back border border-accent-detail rounded-lg p-4 shadow-lg mx-4 w-auto min-w-[200px]">
+      <div class="bg-back border border-accent-detail rounded-lg p-4 shadow-lg mx-4 w-auto min-w-[150px]">
         <div class="flex justify-between">
           <p class="text-lg text-bold text-print">[ Eliminar ]</p>
           <button on:click={cancel_delete} class="text-sm rounded-md bg-back text-accent-detail hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail">
@@ -288,7 +288,7 @@
         </div>
         <div class="flex items-center justify-center gap-2 mb-3 text-accent-detail">
           <Icon icon={delete_target.entry_type === "dir" ? dir_icon() : file_icon(delete_target.name)} class="w-5 h-5 text-accent-detail" />
-          <span class="text-sm truncate text-center">{delete_target.name}?</span>
+          <span class="text-lg truncate text-center">{delete_target.name}?</span>
         </div>
         <div class="flex gap-2 pt-2 justify-between mx-2">
           <button on:click={do_delete} class="px-5 py-1 rounded-md bg-back text-accent-detail hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail text-sm">
