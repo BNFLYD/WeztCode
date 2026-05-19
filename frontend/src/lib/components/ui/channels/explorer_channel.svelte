fc800a62314b84a254b2809e3c431dcd5b80651f
BNFLYD <moralesflaviog@gmail.com>
2026-05-19 11:13:20 -0300

    card style 4

--- frontend/src/lib/components/ui/channels/explorer_channel.svelte
+++ frontend/src/lib/components/ui/channels/explorer_channel.svelte
@@ -9,22 +9,22 @@
 </script>
 
 <div class="absolute inset-0 flex items-center justify-center">
-  <div class="bg-back-deep rounded-xl p-4 shadow-lg mx-4 w-auto min-w-[200px]">
-    <p class="text-sm text-print mb-2">Queres borrar</p>
-    <div class="flex items-center gap-2 mb-3 text-print font-sans mb-4 ml-2">
+  <div class="text-lg text-print font-sans bg-back-deep rounded-xl p-4 shadow-lg mx-4 w-auto min-w-[200px]">
+    <p class="mb-2">Queres borrar</p>
+    <div class="flex items-center gap-2 mb-3 text-print mb-4 ml-2">
       <Icon icon={icon} class="w-5 h-5 text-accent-detail"/>
-      <span class="text-md truncate">{name}?</span>
+      <span class="truncate">{name}?</span>
     </div>
-    <div class="flex px-4 gap-2 justify-between font-sans">
+    <div class="flex px-4 gap-2 justify-between font-sans text-lg">
       <button
         on:click={() => { on_confirm(); on_close(); }}
-        class="px-4 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail text-md"
+        class="px-4 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail"
       >
         Si
       </button>
       <button
         on:click={() => { on_cancel(); on_close(); }}
-        class="px-4 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail text-md"
+        class="px-4 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail"
       >
         No
       </button>
