9715d4e30b95de7615acd83644d40cbfc1736e3e
BNFLYD <moralesflaviog@gmail.com>
2026-05-19 12:37:35 -0300

    distortion

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,19 +11,30 @@
 
   let active_section = "explorer";
   let active_channel = null;
+  let is_distorting = false;
 
   function handle_channel(ch) {
-    active_channel = ch;
+    if (is_distorting) return;
+    is_distorting = true;
+    setTimeout(() => {
+      active_channel = ch;
+      is_distorting = false;
+    }, 600);
   }
 
   function handle_channel_close() {
-    active_channel = null;
+    if (is_distorting) return;
+    is_distorting = true;
+    setTimeout(() => {
+      active_channel = null;
+      is_distorting = false;
+    }, 600);
   }
 </script>
 
 <div class="h-[100vh] flex flex-col bg-back-deep rounded-l-3xl overflow-hidden">
   <div class="px-7 pt-5">
-    <Monitor {active_section} channel={active_channel} on_channel_close={handle_channel_close} on_section_change={(id) => (active_section = id)}  />
+    <Monitor {active_section} channel={active_channel} {is_distorting} on_channel_close={handle_channel_close} on_section_change={(id) => (active_section = id)}  />
   </div>
   <div class="flex-1 overflow-y-auto px-5">
     {#if active_section === "explorer"}
