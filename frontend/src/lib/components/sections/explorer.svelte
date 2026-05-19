789af44c9e2462283e6ee249ed3f7e40e9283831
BNFLYD <moralesflaviog@gmail.com>
2026-05-18 22:28:03 -0300

    message format 10

--- frontend/src/lib/components/sections/explorer.svelte
+++ frontend/src/lib/components/sections/explorer.svelte
@@ -278,8 +278,8 @@
       {/each}
     {/if}
   {#if deleting && delete_target}
-    <div class="">
-      <div class="bg-back border border-accent-detail rounded-lg p-4 shadow-lg mx-4 w-auto min-w-[150px]">
+    <div class="absolute inset-0 flex items-center justify-center bg-black/60 z-20 rounded-lg">
+      <div class="bg-back border border-accent-detail rounded-lg p-4 shadow-lg mx-8 w-auto min-w-[150px]">
         <div class="flex justify-between">
           <p class="text-lg text-bold text-print">[ Eliminar ]</p>
           <button on:click={cancel_delete} class="text-sm rounded-md bg-back text-accent-detail hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail">
