d1837694d8b32e10a28d40143f07b6aef4966000
BNFLYD <moralesflaviog@gmail.com>
2026-05-19 22:04:12 -0300

    btn change bg

--- frontend/src/lib/components/ui/channels/explorer_channel.svelte
+++ frontend/src/lib/components/ui/channels/explorer_channel.svelte
@@ -6,25 +6,39 @@
   export let on_confirm = () => {};
   export let on_cancel = () => {};
   export let on_close = () => {};
+
+  let selected = "si";
 </script>
 
 <div class="absolute inset-0 flex items-center justify-center">
-  <div class="text-lg text-print font-sans bg-back-deep rounded-xl p-4 shadow-lg mx-7 w-auto min-w-[200px]">
+  <div class="text-lg text-print font-sans bg-back-deep rounded-lg p-4 shadow-lg mx-7 w-auto min-w-[200px]">
     <p class="mb-1">Queres borrar</p>
     <div class="flex items-center gap-2 text-print mb-4 ml-2">
       <Icon icon={icon} class="w-5 h-5 text-accent-detail"/>
       <span class="font-bold truncate">{name}?</span>
     </div>
-    <div class="flex px-4 gap-2 justify-between font-sans text-md">
+    <div class="flex px-2 gap-2 justify-between font-sans font-bold text-sm">
       <button
         on:click={() => { on_confirm(); on_close(); }}
-        class="px-5 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail"
+        on:mouseenter={() => selected = "si"}
+        on:mouseleave={() => selected = "si"}
+        class="px-5 rounded-sm transition-colors border border-accent-detail"
+        class:bg-accent-detail={selected === "si"}
+        class:text-back={selected === "si"}
+        class:bg-transparent={selected !== "si"}
+        class:text-print={selected !== "si"}
       >
         Si
       </button>
       <button
         on:click={() => { on_cancel(); on_close(); }}
-        class="px-5 rounded-md bg-back text-print hover:bg-accent-detail hover:text-back transition-colors border border-accent-detail"
+        on:mouseenter={() => selected = "no"}
+        on:mouseleave={() => selected = "si"}
+        class="px-5 rounded-sm transition-colors border border-accent-detail"
+        class:bg-accent-detail={selected === "no"}
+        class:text-back={selected === "no"}
+        class:bg-transparent={selected !== "no"}
+        class:text-print={selected !== "no"}
       >
         No
       </button>
