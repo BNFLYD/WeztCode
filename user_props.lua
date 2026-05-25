3a14587980807bb0555606fa1c1077921b040d44
BNFLYD <moralesflaviog@gmail.com>
2026-05-25 14:24:56 -0300

    keys test

--- user_props.lua
+++ user_props.lua
@@ -5,6 +5,6 @@ user_editor = "nvim"
 current_dir = "/home/mori/Projects/Rust/GTK4/WeztCode"
 
 -- Keybindings de navegación entre tabs (opcional)
--- Descomentar para overridear los defaults:
+-- Descomentar para overridear los defaults
 tab_next = "CTRL+J"
 tab_prev = "CTRL+K"
