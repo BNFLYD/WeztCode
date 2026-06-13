f13cb628ecc5b42f478f7959066714ec7ace08ce
BNFLYD <moralesflaviog@gmail.com>
2026-06-12 23:30:17 -0300

    idk 8

--- frontend/src/lib/components/sections/terminal.svelte
+++ frontend/src/lib/components/sections/terminal.svelte
@@ -347,8 +347,8 @@
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y_no_static_element_interactions -->
         <div
-          class={"bg-transparent flex items-center gap-2 px-3 py-2.5 rounded-lg transition-colors cursor-pointer" +
-            (cursor_index === index ? "bg-accent/10 hover:bg-accent/10" : "hover:bg-accent/5")}
+          class={"flex items-center gap-2 px-3 py-2.5 rounded-lg hover:bg-accent/5 transition-colors cursor-pointer" +
+            (cursor_index === index ? "bg-accent/10 hover:bg-accent/10" : "")}
           title={pane.title || `Pane ${pane.pane_id}`}
           data-index={index}
           on:click={() => {
