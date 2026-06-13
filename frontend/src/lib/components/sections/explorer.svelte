9acf4a059985710c270389f9f947f63e62da0e07
BNFLYD <moralesflaviog@gmail.com>
2026-06-12 23:16:26 -0300

    idk

--- frontend/src/lib/components/sections/explorer.svelte
+++ frontend/src/lib/components/sections/explorer.svelte
@@ -663,7 +663,7 @@
         {#each projects as proj, index (proj.path)}
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <div class={"flex items-center gap-2 px-3 py-2.5 rounded-lg transition-colors text-lg group cursor-pointer"
-              + (cursor_index === index ? " bg-accent/10" : "hover:bg-accent/5")}
+              + (cursor_index === index ? "bg-accent/10" : "hover:bg-accent/5")}
             data-index={index}
             on:click={() => {
               cursor_index = index;
